25900e8988cccd9fd19c02c629e9a82b1169f350
Joao Sa <joao.sa@innvent.com.br>
2013-08-12 12:12:40 -0300

    Add initial capistrano recipes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "http://rubygems.org"
 
+gem "capistrano"
+
 group :development do
   gem "jeweler", "~> 1.8.4"
 end
---
+++ lib/recipes/unicorn.rb
@@ -0,0 +1,14 @@
+set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
+set_default(:unicorn_workers, 1)
+set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
+
+namespace :unicorn do
+
+  desc "Initial Setup"
+  task :setup, :roles => [:app, :dj] do
+    run "mkdir -p #{shared_path}/config"
+    template "unicorn.rb.erb", unicorn_config
+  end
+  after "deploy:setup", "unicorn:setup"
+
+end
