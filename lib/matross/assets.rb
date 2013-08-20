a18176529ea68e8176d220b18c01364091093b72
Joao Sa <joao.sa@innvent.com.br>
2013-08-20 13:39:35 -0300

    Specify rails_env for asset precompilation

--- lib/matross/assets.rb
+++ lib/matross/assets.rb
@@ -4,12 +4,20 @@ namespace :deploy do
   namespace :assets do
     desc "Run Rails asset pipeline locally"
     task :precompile, :roles => :web do
-      run_locally "#{rake} assets:clean && #{rake} assets:precompile"
-      run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+      run_locally <<-CMD.compact
+        RAILS_ENV=#{rails_env.to_s.shellescape} #{rake} assets:clean &&
+        RAILS_ENV=#{rails_env.to_s.shellescape} #{rake} assets:precompile &&
+        cd public && tar -jcf assets.tar.bz2 assets
+      CMD
       top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
       run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-      run_locally "rm public/assets.tar.bz2"
-      run_locally "#{rake} assets:clean"
+
+      # Clean up temporary local assets
+      run_locally <<-CMD.compact
+        rm public/assets.tar.bz2 &&
+        RAILS_ENV=#{rails_env.to_s.shellescape} #{rake} assets:clean
+      CMD
     end
+
   end
 end
