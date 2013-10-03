76b73efc7f7fa225ac95f1a37759254389fd5b83
Joao Sa <joao.sa@innvent.com.br>
2013-10-03 12:35:32 -0300

    Use rails_env set by .env in dj
    - Remove needless declaration of same environment value twice.

--- lib/matross/delayed_job.rb
+++ lib/matross/delayed_job.rb
@@ -5,7 +5,7 @@ namespace :delayed_job do
   desc "Writes the delayed job part of the Procfile"
   task :procfile, :roles => :dj do
     procfile_template = <<-EOF.gsub(/^\s+/, '')
-      dj: RAILS_ENV=<%= rails_env %> bundle exec rake jobs:work
+      dj: bundle exec rake jobs:work
     EOF
     procfile = ERB.new(procfile_template, nil, '-')
     put procfile.result(binding), "#{shared_path}/Procfile.dj"
