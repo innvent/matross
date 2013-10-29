59c04064f0a0d63869d4b972245bf2de1293f31f
Artur Rodrigues <arturhoo@gmail.com>
2013-10-29 16:45:47 -0200

    Remove leading whitespace from DJ foreman task

--- lib/matross/delayed_job.rb
+++ lib/matross/delayed_job.rb
@@ -7,13 +7,13 @@ namespace :delayed_job do
   desc "Writes the delayed job part of the Procfile"
   task :procfile, :roles => :dj do
     procfile_template = <<-EOF.gsub(/^\s+/, '')
-      <% if dj_queues -%>
-        <% dj_queues.each do |queue_name| -%>
+      <%- if dj_queues -%>
+        <%- dj_queues.each do |queue_name| -%>
           dj_<%= queue_name %>: bundle exec rake jobs:work QUEUE=<%= queue_name %>
-        <% end -%>
-      <% else -%>
+        <%- end -%>
+      <%- else -%>
         dj: bundle exec rake jobs:work
-      <% end -%>
+      <%- end -%>
     EOF
     procfile = ERB.new(procfile_template, nil, '-')
     put procfile.result(binding), "#{shared_path}/Procfile.dj"
