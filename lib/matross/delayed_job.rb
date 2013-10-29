be0d0e2f63ffb6db237101ee8de71e3bacf03887
Artur Rodrigues <arturhoo@gmail.com>
2013-10-29 14:45:50 -0200

    Allow definition of DJ queues

--- lib/matross/delayed_job.rb
+++ lib/matross/delayed_job.rb
@@ -5,7 +5,13 @@ namespace :delayed_job do
   desc "Writes the delayed job part of the Procfile"
   task :procfile, :roles => :dj do
     procfile_template = <<-EOF.gsub(/^\s+/, '')
-      dj: bundle exec rake jobs:work
+      <% if queues -%>
+        <% queues.each do |queue_name| -%>
+          dj_<%= queue_name %>: bundle exec rake jobs:work QUEUE=<%= queue_name %>
+        <% end -%>
+      <% else -%>
+        dj: bundle exec rake jobs:work
+      <% end -%>
     EOF
     procfile = ERB.new(procfile_template, nil, '-')
     put procfile.result(binding), "#{shared_path}/Procfile.dj"
