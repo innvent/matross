ad700cb44c5bb623eedb4cb7c1af162de31e8d37
Artur Rodrigues <arturhoo@gmail.com>
2013-10-30 12:09:21 -0200

    Add a faye.ru configuration for proper execution

--- lib/matross/faye.rb
+++ lib/matross/faye.rb
@@ -2,6 +2,7 @@ dep_included? 'faye'
 dep_included? 'thin'
 
 _cset(:faye_config) { "#{shared_path}/config/faye_server.yml" }
+_cset(:faye_ru)     { "#{shared_path}/config/faye.ru" }
 
 namespace :faye do
 
@@ -9,6 +10,7 @@ namespace :faye do
   task :setup, :roles => :app do
     run "mkdir -p #{shared_path}/config"
     template "faye/faye_server.yml.erb", faye_config
+    template "faye/faye.ru.erb", faye_ru
   end
   after "deploy:setup", "faye:setup"
 
@@ -21,7 +23,7 @@ namespace :faye do
   desc "Writes the faye part of the Procfile"
   task :procfile, :roles => :faye do
     procfile_template = <<-EOF.gsub(/^\s+/, '')
-      faye: bundle exec rackup  <%= faye_config %> -s thin -E <%= rails_env %> -p <%= faye_port %>
+      faye: bundle exec rackup  <%= faye_ru %> -s thin -E <%= rails_env %> -p <%= faye_port %>
     EOF
     procfile = ERB.new(procfile_template, nil, '-')
     put procfile.result(binding), "#{shared_path}/Procfile.faye"
