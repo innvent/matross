d99f8295ce87f0accd53e9d8178ed75646993019
Artur Rodrigues <arturhoo@gmail.com>
2013-11-18 11:59:14 -0200

    Define a per-application socket
    This allows multiple apps to be deployed to the same machine

--- lib/matross/nginx.rb
+++ lib/matross/nginx.rb
@@ -5,7 +5,6 @@ namespace :nginx do
     template "nginx/nginx_virtual_host_conf.erb", "/tmp/#{application}"
     run "#{sudo} mv /tmp/#{application} /etc/nginx/sites-available/#{application}"
     run "#{sudo} ln -fs /etc/nginx/sites-available/#{application} /etc/nginx/sites-enabled/#{application}"
-    run "mkdir -p #{shared_path}/sockets"
   end
   after "deploy:setup", "nginx:setup"
 
