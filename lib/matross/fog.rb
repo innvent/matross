bdea56bdd8167343ad098d7ecc2fb6fedb610c69
Artur Rodrigues <arturhoo@gmail.com>
2013-12-19 11:56:43 -0200

    Fog tasks now run on DJ hosts

--- lib/matross/fog.rb
+++ lib/matross/fog.rb
@@ -1,21 +1,21 @@
-dep_included? "fog"
+dep_included? 'fog'
 
 _cset(:fog_config)  { "#{shared_path}/config/fog_config.yml" }
-_cset :fog_region,  "us-east-1"
+_cset :fog_region,  'us-east-1'
 _cset :fog_public,  false
 
 namespace :fog do
 
-  desc "Creates the fog_config.yml in shared path"
-  task :setup, :roles => :app do
+  desc 'Creates the fog_config.yml in shared path'
+  task :setup, :roles => [:app, :dj] do
     run "mkdir -p #{shared_path}/config"
-    template "fog/fog_config.yml.erb", fog_config
+    template 'fog/fog_config.yml.erb', fog_config
   end
-  after "deploy:setup", "fog:setup"
+  after 'deploy:setup', 'fog:setup'
 
-  desc "Updates the symlink for fog_config.yml for deployed release"
-  task :symlink, :roles => :app do
+  desc 'Updates the symlink for fog_config.yml for deployed release'
+  task :symlink, :roles => [:app, :dj] do
     run "ln -nfs #{fog_config} #{release_path}/config/fog_config.yml"
   end
-  after "bundle:install", "fog:symlink"
+  after 'bundle:install', 'fog:symlink'
 end
