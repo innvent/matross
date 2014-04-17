e9e75aad32f62622d9ecd63754f39489580c25b9
Artur Rodrigues <arturhoo@gmail.com>
2014-04-17 11:52:07 -0300

    Extend postgresql support

--- lib/matross/postgresql.rb
+++ lib/matross/postgresql.rb
@@ -1,19 +1,44 @@
 dep_included? 'pg'
 
-_cset(:database_config)             { "#{shared_path}/config/database.yml" }
+_cset(:database_config) { "#{shared_path}/config/database.yml" }
+_cset(:postgresql_user) { user }
 
 namespace :postgresql do
 
-  desc "Creates the database.yml file in shared path"
+  desc 'Creates the database.yml file in shared path. User is created if it does not exist'
   task :setup, :roles => [:app, :dj] do
     run "mkdir -p #{shared_path}/config"
     template "postgresql/database.yml.erb", database_config
+    user_count = capture(%W{ sudo -u postgres psql postgres -tAc
+                             "SELECT 1 FROM pg_roles
+                              WHERE rolname='#{postgresql_user}'" } * ' ').to_i
+    if user_count == 0
+      run "#{sudo} -u postgres createuser -d -r -s #{postgresql_user}"
+    else
+      logger.info 'User already created, skpping'
+    end
   end
-  after "deploy:setup", "postgresql:setup"
+  after 'deploy:setup', 'postgresql:setup'
 
-  desc "Updates the symlink for database.yml for deployed release"
+  desc 'Updates the symlink for database.yml for deployed release'
   task :symlink, :roles => [:app, :dj] do
     run "ln -nfs #{database_config} #{release_path}/config/database.yml"
   end
-  after "bundle:install", "postgresql:symlink"
+  after 'bundle:install', 'postgresql:symlink'
+
+  desc "Creates the database and loads the schema"
+  task :create, :roles => :db do
+    db_count = capture(%W{ #{sudo} -u postgres psql -lqt |
+                           cut -d \\| -f 1 |
+                           grep -w #{postgresql_database.gsub("-", "_")} |
+                           wc -l } * ' ').to_i
+    if db_count == 0
+      run %W{ cd #{release_path} &&
+              RAILS_ENV=#{rails_env.to_s.shellescape}
+              bundle exec rake db:create db:schema:load } * ' '
+    else
+      logger.info 'DB is already configured, skipping'
+    end
+  end
+  after 'postgresql:symlink', 'postgresql:create'
 end
